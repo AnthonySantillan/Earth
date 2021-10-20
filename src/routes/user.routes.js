091bf3d8e057da02e5c8017944c561fb90a2bfed
AnthonySantillan <58825544+AnthonySantillan@users.noreply.github.com>
2021-10-20 16:18:05 -0500

    obtain data of users

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,32 +1,32 @@
-const express= require("express");
+const express = require("express");
 const router = express.Router();
-const userModel = require ("../models/user");
+const userModel = require("../models/user");
 const app = express();
 const user = require('../controllers/user.controller');
 
 const jwt = require("jsonwebtoken");
 
-router.post('/register', async (req,res)=>{
-    const{name,lastname,card,email,password}= req.body;
-    const newUser = new userModel({name, lastname, card, email, password});
+router.post('/register', async (req, res) => {
+    const { name, lastname, card, email, password } = req.body;
+    const newUser = new userModel({ name, lastname, card, email, password });
     await newUser.save();
-    const token = await jwt.sign({_id: newUser._id},'secretkey');
-    res.status(200).json({token});
+    const token = await jwt.sign({ _id: newUser._id }, 'secretkey');
+    res.status(200).json({ token });
 });
 
-router.post('/login', async (req,res)=>{
-    const{email, password}= req.body;
-    const user = await userModel.findOne({email});
-    if(!user) return res.status(401).send('email does not exist');
-    if(user.password !== password) return res.status(401).send('password error');
-    const token = await jwt.sign({_id: user._id},'secretkey');
-    res.status(200).json({token});
+router.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+    const user = await userModel.findOne({ email });
+    if (!user) return res.status(401).send('email does not exist');
+    if (user.password !== password) return res.status(401).send('password error');
+    const token = await jwt.sign({ _id: user._id }, 'secretkey');
+    res.status(200).json({ token, user });
 });
 
-router.get("/users",user.getUsers);
+router.get("/users", user.getUsers);
 
-router.get("/users/:id",user.getUser);
+router.get("/users/:id", user.getUser);
 
-router.delete("/users/:id",user.deleteUser);
+router.delete("/users/:id", user.deleteUser);
 
 module.exports = router;
