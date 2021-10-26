8d6e97d280da814722f01f8a9cc20ccdd88dbd0b
Francis9817 <franciscvalladares@gmail.com>
2021-10-26 13:56:31 -0500

    user profile update

--- src/app/pages/administrador/admin-usuarios/admin-usuarios.component.ts
+++ src/app/pages/administrador/admin-usuarios/admin-usuarios.component.ts
@@ -1,6 +1,8 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
+import { User } from 'src/app/models/user.model';
 import { Usuario } from 'src/app/models/usuarios.models';
 import { UserService } from 'src/app/services/user.service';
 import { UsuariosService } from 'src/app/services/usuarios.service';
@@ -126,6 +128,60 @@ export class AdminUsuariosComponent implements OnInit {
       }
     );
   }
+
+  addUser(form?: NgForm) {
+    if (form.value._id) {
+      Swal.fire('Admin', `Se actualizo exitosamente`, 'success');
+
+      this.userService.putUser(form.value).subscribe((res) => {
+        this.resetForm(form);
+        this.getUsers();
+      }, (err) => {
+        console.log(err)
+        Swal.fire('', ``, 'error');
+      });
+    } else {
+      Swal.fire('Administrador', `Edite un Usuario para poder actualizar`, 'error');
+    }
+  }
+  resetForm(form?: NgForm) {
+    if (form) {
+      form.reset();
+      this.userService.selectedUser = new User();
+    }
+  }
+
+  editUser(user: User) {
+    this.userService.selectedUser = user;
+  }
+
+  deleteUser(_id: string, form: NgForm) {
+
+    Swal
+      .fire({
+        title: "Admin",
+        text: "¿Estas seguro de eliminar el usuario?",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonText: "Sí, eliminar",
+        cancelButtonText: "Cancelar",
+      })
+      .then(resultado => {
+        if (resultado.value) {
+          // Hicieron click en "Sí"
+          console.log("se elimina el usuario");
+          Swal.fire('Admin', `Se borro exitosamente`, 'success');
+          this.userService.deleteUser(_id).subscribe((res) => {
+            this.getUsers();
+            this.resetForm(form);
+          });
+        } else {
+          // Dijeron que no
+          console.log("*NO se elimina el usuario");
+        }
+      });
+  }
+
 
 }
 
