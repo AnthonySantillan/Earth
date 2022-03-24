69c87ecfd11520bbe3a8c36eb201804582b7c4c1
1727326520 <ferchofercho933@gmail.com>
2022-03-24 10:18:23 -0500

    changes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/',(req,res) => {
-    res.send('hola mundo');
+router.get('/', async (req, res) => {
+    res.render('partials/index');
 });
 
 module.exports = router;
--- src/routes/links.js
+++ src/routes/links.js
@@ -1,4 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const pool = require('../config/database');
 
+router.get('/user', (req,res) => {
+    res.render('users/add');
+});
+router.post('/user', (req,res) => {
+    res.send('receive');
+});
 module.exports = router;
