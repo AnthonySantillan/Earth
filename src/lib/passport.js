4ccc4f79d512b33166822a7bbb21596036b121ed
1727326520 <ferchofercho933@gmail.com>
2022-03-25 13:59:42 -0500

    changes

--- src/lib/passport.js
+++ src/lib/passport.js
@@ -5,19 +5,28 @@ const orm = require('../config/dataBase.orm');
 const pool = require('../config/dataBase');
 const helpers = require("../lib/helpers");
 
-passport.use(
-  "local.signin",
-  new LocalStrategy(
-    {
-      usernameField: "email",
-      passwordField: "password",
-      passReqToCallback: true
-    },
-    async (req, email, password, done) => {
 
+passport.use('local.signin', new LocalStrategy({
+  usernameField: 'email',
+  passwordField: 'password',
+  passReqToCallback: true
+}, async (req, email, password, done) => {
+  const rows = await pool.query('SELECT * FROM users WHERE email = ?', [email]);
+
+  if (rows.length > 0) {
+    const user = rows[0];
+    const validPassword = await helpers.matchPassword(password, user.password)
+    console.log(validPassword);
+
+    if (validPassword) {
+      done(null, user, req.flash('success', 'Welcome ' + user.email));
+    } else {
+      done(null, false, req.flash('message', 'Incorrect Password'));
     }
-  )
-);
+  } else {
+    return done(null, false, req.flash('message', 'The Username does not exists.'));
+  }
+}));
 
 passport.use(
   "local.signup",
--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -3,13 +3,30 @@ const router = express.Router();
 const passport = require('passport');
 
 const pool = require('../config/database');
+const { isLoggedIn } = require('../lib/auth');
 
-router.get('/signup', (req, res) => {
-    res.render('auth/signup');
+
+
+  router.get('/signin', (req, res) => {
+    res.render('auth/signin');
   });
 
+  router.post('/signin', passport.authenticate('local.signin', {
+ 
+    
+      successRedirect: '/profile',
+      failureRedirect: '/signin',
+      failureFlash: true
+    
+  }));
+
+
+  router.get('/signup', (req, res) => {
+    res.render('auth/signup');
+  });
+  
   router.post('/signup', passport.authenticate('local.signup', {
-    successRedirect: '/profile',
+    successRedirect: '/signin',
     failureRedirect: '/signup',
     failureFlash: true
   }));
